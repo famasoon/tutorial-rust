ef97eaf5b5e07368234e4c87bf45405231eb6878
FAMASoon <famasoon@gmail.com>
2022-04-28 20:47:07 +0900

    メソッドを使用するように変更

--- ch5/rectangles/src/main.rs
+++ ch5/rectangles/src/main.rs
@@ -2,12 +2,15 @@ struct Rectangle {
     width: u32,
     height: u32,
 }
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50};
 
-    println!("The area of the rectangle is {} square pixels.", area(&rect1));
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
+    println!("The area of the rectangle is {} square pixels.", rect1.area());
 }
