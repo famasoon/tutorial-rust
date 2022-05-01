42b30b733a23cae18e0e4506ece20eb18d06f0f5
FAMASoon <famasoon@gmail.com>
2022-05-01 20:20:56 +0900

    superを使用し関数へのパスを指定

--- ch7/restrant/src/lib.rs
+++ ch7/restrant/src/lib.rs
@@ -7,6 +7,13 @@ mod front_of_house {
     mod serving {
         fn take_order() {}
         fn serve_order() {}
+        mod back_of_house {
+            fn fix_incorrect_order() {
+                cook_order();
+                super::serve_order();
+            }
+            fn cook_order() {}
+        }
         fn take_payment() {}
     }
 }
