1bb6a956463087f5510150d6d8d02e34e72c7eea
FAMASoon <famasoon@gmail.com>
2022-05-01 20:30:12 +0900

    公開非公開の構造体を用いた実装

--- ch7/restrant/src/lib.rs
+++ ch7/restrant/src/lib.rs
@@ -13,12 +13,29 @@ mod front_of_house {
                 super::serve_order();
             }
             fn cook_order() {}
+
+            pub struct  Breakfast {
+                pub toast: String,
+                seasonal_fruit: String,
+            }
+
+            impl Breakfast {
+                pub fn summer(toast: &str) -> Breakfast {
+                    Breakfast {
+                        toast: String::from(toast),
+                        seasonal_fruit: String::from("peaches"),
+                    }
+                }
+            }
         }
+
+        pub fn eat_at_restaurant() {
+            let mut meal = back_of_house::Breakfast::summer("Rye");
+            meal.toast = String::from("Wheat");
+
+            println!("I'd like {} toast please", meal.toast);
+        }
+
         fn take_payment() {}
     }
-}
-
-pub fn eat_at_restraurant() {
-    crate::front_of_house::hosting::add_to_waitlist();
-    front_of_house::hosting::add_to_waitlist();
 }
