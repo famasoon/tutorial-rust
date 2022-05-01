009ba4f9ea3c634f6a280a904fd1e3137483aaa8
FAMASoon <famasoon@gmail.com>
2022-05-01 20:32:38 +0900

    enumをmod配下で作成

--- ch7/restrant/src/lib.rs
+++ ch7/restrant/src/lib.rs
@@ -27,9 +27,16 @@ mod front_of_house {
                     }
                 }
             }
+
+            pub enum Appetizer {
+                Soup,
+                Salad,
+            }
         }
 
         pub fn eat_at_restaurant() {
+            let order1 = back_of_house::Appetizer::Soup;
+            let order2 = back_of_house::Appetizer::Salad;
             let mut meal = back_of_house::Breakfast::summer("Rye");
             meal.toast = String::from("Wheat");
 
