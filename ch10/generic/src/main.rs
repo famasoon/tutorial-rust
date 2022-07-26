49afd36563101a2d6ea10369392eede135c39ab4
FAMASoon <famasoon@gmail.com>
2022-07-26 21:13:55 +0900

    ジェネリクスを使ってみた

--- ch10/generic/src/main.rs
+++ ch10/generic/src/main.rs
@@ -1,21 +1,12 @@
-fn largest(list:  &[i32]) -> i32 {
-    let mut largest = list[0];
-
-    for &item in list.iter() {
-        if item >  largest {
-            largest = item;
-        }
-    }
-
-    largest
+#[derive(Debug)]
+struct Point<T> {
+    x: T,
+    y: T,
 }
 
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    let integer = Point { x: 5, y: 10};
+    let float = Point { x: 1.0, y: 4.0};
 
-    let number_list = vec![102, 34, 6000, 89, 54, 2, 43, 8];
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
+    println!("INT: {:?}, FLOAT: {:?}", integer, float);
 }
