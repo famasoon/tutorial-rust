5fa3e67ab9d9f782e0b2fff9a83bb3c3f19d0ab6
FAMASoon <famasoon@gmail.com>
2022-07-28 20:15:08 +0900

    トレイトで定義した関数を呼び出してみる

--- ch10/traittutorial/src/main.rs
+++ ch10/traittutorial/src/main.rs
@@ -1,3 +1,13 @@
+mod lib;
+
+use crate::lib::{Tweet, Summary};
+
 fn main() {
-    println!("Hello, world!");
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course"),
+        reply: false,
+        retweet: false,
+    };
+    println!("1 new tweet: {}", tweet.summarize());
 }
