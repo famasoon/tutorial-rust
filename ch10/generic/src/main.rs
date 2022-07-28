d8c025d3d233294967769a47db9d080922b050b2
FAMASoon <famasoon@gmail.com>
2022-07-28 21:03:02 +0900

    トレイト境界を使用してlargestを呼び出す

--- ch10/generic/src/main.rs
+++ ch10/generic/src/main.rs
@@ -1,21 +1,23 @@
-struct Point<T, U> {
-    x: T,
-    y: U,
-}
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
 
-impl<T, U> Point<T, U> {
-    fn mixup<V, W>(self, other: Point<V, W>) -> Point<T, W> {
-        Point {
-            x: self.x,
-            y: other.y
+    for &item in list {
+        if item > largest {
+            largest = item;
         }
     }
+
+    largest
 }
 
 fn main() {
-    let p1 = Point { x: 5, y: 10};
-    let p2 = Point { x: "Hello", y: 'c'};
+    let number_list = vec![34, 50, 25, 100, 65];
 
-    let p3 = p1.mixup(p2);
-    println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
 }
