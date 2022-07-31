1c509bcca85ce87218afda78b799821bd993d2d2
FAMASoon <famasoon@gmail.com>
2022-07-31 21:58:30 +0900

    コネクションをハンドルする

--- ch20/hello/src/main.rs
+++ ch20/hello/src/main.rs
@@ -1,4 +1,6 @@
+use std::io::prelude::*;
 use std::net::TcpListener;
+use std::net::TcpStream;
 
 fn main() {
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
@@ -6,6 +8,12 @@ fn main() {
     for stream in listner.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established!");
+        handle_connection(stream);
     }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+    stream.read(&mut buffer).unwrap();
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 }
